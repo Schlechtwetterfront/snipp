acdbc8fa82d666806516b673ec411b63837a5936
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-06-27 12:54:52 +0200

    Add design time mock clip list

---
+++ ViewModels/MockClipList.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clipman.ViewModels
+{
+    class MockClipList : ClipListViewModel
+    {
+        public MockClipList()
+        {
+            AddClip(new Clipboard.Clip("Clip 1"));
+            AddClip(new Clipboard.Clip("Clip 2"));
+            AddClip(new Clipboard.Clip("Clip 3"));
+        }
+    }
+}
