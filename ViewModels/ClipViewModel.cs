ad239de9f199a0e4d513e0d0cc2124c69c038ae6
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-06-27 10:02:59 +0200

    Add ClipViewModel

---
+++ ViewModels/ClipViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clipman.ViewModels
+{
+    public class ClipViewModel
+    {
+        protected Clipboard.Clip clip;
+        public Clipboard.Clip Clip
+        {
+            get
+            {
+                return clip;
+            }
+            set
+            {
+                clip = value;
+            }
+        }
+
+        public String CreatedString
+        {
+            get
+            {
+                if (clip.Created.Date == DateTime.Today)
+                {
+                    return clip.Created.ToShortTimeString();
+                }
+                else
+                {
+                    return clip.Created.ToString("d");
+                }
+            }
+        }
+
+        public ClipViewModel(Clipboard.Clip clip)
+        {
+            this.clip = clip;
+        }
+    }
+}
