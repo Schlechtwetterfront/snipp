6db6cab3f369157648adecd6cfce458b905794a5
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-07-08 20:28:48 +0200

    Remove obsolete clip clear request code

--- ViewModels/SettingsPanelViewModel.cs
+++ ViewModels/SettingsPanelViewModel.cs
@@ -4,11 +4,5 @@ namespace clipman.ViewModels
 {
     class SettingsPanelViewModel
     {
-        public void ClearClips()
-        {
-            ClearRequested?.Invoke(this, EventArgs.Empty);
-        }
-
-        public event EventHandler ClearRequested;
     }
 }
