087d2016acfdc3324815fd6852dac1c47079fdd7
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-07-09 21:06:30 +0200

    Implement SettingsPanelViewModel

--- ViewModels/SettingsPanelViewModel.cs
+++ ViewModels/SettingsPanelViewModel.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace clipman.ViewModels
 {
-    class SettingsPanelViewModel
+    class SettingsPanelViewModel : INotifyPropertyChanged
     {
+        private Settings.Settings settings;
+        public Settings.Settings Settings
+        {
+            get { return settings; }
+            private set { settings = value; RaisePropertyChanged("Settings"); }
+        }
+
+        public SettingsPanelViewModel(Settings.Settings settings)
+        {
+            this.Settings = settings;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
