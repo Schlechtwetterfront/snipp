03ae5b4b5fabaaf94ff2446256fda8c86828d5ba
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-08-02 17:15:05 +0200

    Make repo link clickable

--- Views/SettingsPanel.xaml.cs
+++ Views/SettingsPanel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,12 @@ namespace clipman.Views
             InitializeComponent();
 
             TextOptions.SetTextFormattingMode(this, TextFormattingMode.Display);
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            Process.Start(e.Uri.AbsoluteUri);
+            e.Handled = true;
         }
     }
 }
