c0d317fa521c88665e86d12f87630d61fbe64984
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-08-17 17:37:45 +0200

    Serialize Clips as xml to get rid of json lib dependency

--- Utility/Storage.cs
+++ Utility/Storage.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace clipman.Utility
 {
     public static class Storage
     {
-        static String SnippetFileName = "snippets.json";
+        static String SnippetFileName = "snippets.xml";
 
         public static void SaveSnippets(IEnumerable<Clipboard.Clip> clips)
         {
-            var json = Newtonsoft.Json.JsonConvert.SerializeObject(clips);
+            var serializer = new XmlSerializer(typeof(List<Clipboard.Clip>));
+            var xml = "";
+
+            using (var writer = new StringWriter())
+            {
+                serializer.Serialize(writer, clips.ToList());
+                xml = writer.ToString();
+            }
 
             IsolatedStorageFile isoStore = IsolatedStorageFile.GetStore(IsolatedStorageScope.User | IsolatedStorageScope.Assembly, null, null);
             using (IsolatedStorageFileStream isoStream = new IsolatedStorageFileStream(SnippetFileName, FileMode.Create, isoStore))
             {
                 using (StreamWriter writer = new StreamWriter(isoStream))
                 {
-                    writer.WriteLine(json);
+                    writer.WriteLine(xml);
                 }
             }
         }
 
         public static List<Clipboard.Clip> RetrieveClips()
         {
+            var serializer = new XmlSerializer(typeof(List<Clipboard.Clip>));
             List<Clipboard.Clip> clips = new List<Clipboard.Clip>();
 
             try
@@ -34,21 +45,13 @@ namespace clipman.Utility
                 {
                     using (StreamReader reader = new StreamReader(isoStream))
                     {
-                        var clipString = reader.ReadToEnd();
-                        if (!string.IsNullOrWhiteSpace(clipString))
-                        {
-                            clips = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Clipboard.Clip>>(clipString);
-                        }
+                        clips = (List<Clipboard.Clip>)serializer.Deserialize(reader);
                     }
                 }
             }
             catch (FileNotFoundException)
             {
-                Utility.Logging.Log("snippets.json not found");
-            }
-            catch (Newtonsoft.Json.JsonReaderException)
-            {
-                Utility.Logging.Log("snippets.json found but has invalid json format");
+                Utility.Logging.Log("snippets.xml not found");
             }
 
             return clips;
